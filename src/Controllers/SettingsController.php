1998f8492a0a5999e9282d89c51862e0daab551a
Ademola <styccs@gmail.com>
2016-11-28 12:05:14 +0100

    updating app

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -20,7 +20,7 @@ class SettingsController extends BaseController
     {
         $files = Files::where('tag', 'prompt')->get();
 
-        $setting = Settings::where('id', 4)->first();
+        $setting = Settings::where('default', true)->first();
 
         return $this->view->render($response, 'templates/forms/settings.twig', [
             'files' => $files,
@@ -39,7 +39,14 @@ class SettingsController extends BaseController
                 'incorrect_path' => $request->getParam('incorrect_path'),
                 'repeat_path' => $request->getParam('repeat_path'),
                 'confirmation_path' => $request->getParam('confirmation_path'),
-                'goodbye_path' => $request->getParam('goodbye_path')
+                'goodbye_path' => $request->getParam('goodbye_path'),
+                'wrong_path' => $request->getParam('wrong_path'),
+                'success_path' => $request->getParam('success_path'),
+                'no_selection_path' => $request->getParam('no_selection_path'),
+                'selection_confirmation_path' => $request->getParam('selection_confirmation_path'),
+                'default' => true,
+                'subscription_failure_path' => $request->getParam('subscription_failure_path'),
+                'continue_path' => $request->getParam('continue_path')
             ]);
         } else {
             $settings = Settings::create([
@@ -47,7 +54,14 @@ class SettingsController extends BaseController
                 'incorrect_path' => $request->getParam('incorrect_path'),
                 'repeat_path' => $request->getParam('repeat_path'),
                 'confirmation_path' => $request->getParam('confirmation_path'),
-                'goodbye_path' => $request->getParam('goodbye_path')
+                'goodbye_path' => $request->getParam('goodbye_path'),
+                'wrong_path' => $request->getParam('wrong_path'),
+                'success_path' => $request->getParam('success_path'),
+                'no_selection_path' => $request->getParam('no_selection_path'),
+                'selection_confirmation_path' => $request->getParam('selection_confirmation_path'),
+                'default' => true,
+                'subscription_failure_path' => $request->getParam('subscription_failure_path'),
+                'continue_path' => $request->getParam('continue_path')
             ]);
         }
 
@@ -63,13 +77,38 @@ class SettingsController extends BaseController
         $settings_command = 'cp ' . $settings->goodbye_path . ' ' . "/var/lib/asterisk/sounds/defaults/goodbye.wav";
         shell_exec($settings_command);
 
+        $wrong_command = 'cp ' . $settings->wrong_path . ' ' . "/var/lib/asterisk/sounds/defaults/wrong.wav";
+        shell_exec($wrong_command);
+
+        $success_command = 'cp ' . $settings->success_path . ' ' . "/var/lib/asterisk/sounds/defaults/success.wav";
+        shell_exec($success_command);
+
+        $no_selection_command = 'cp ' . $settings->no_selection_path . ' ' . "/var/lib/asterisk/sounds/defaults/no_selection.wav";
+        shell_exec($no_selection_command);
+
+        $select_command = 'cp ' . $settings->selection_confirmation_path . ' ' . "/var/lib/asterisk/sounds/defaults/selection_confirmation.wav";
+        shell_exec($select_command);
+
+        $failure_command = 'cp ' . $settings->subscription_failure_path . ' ' . "/var/lib/asterisk/sounds/defaults/failure.wav";
+        shell_exec($failure_command);
+
+        $continue_command = 'cp ' . $settings->continue_path . ' ' . "/var/lib/asterisk/sounds/defaults/continue.wav";
+        shell_exec($continue_command);
+
         Index::index('settings', [
                 'id' => $settings->id,
                 'advert_limit' => $settings->advert_limit,
                 'incorrect_path' => '/var/lib/asterisk/sounds/defaults/incorrect.wav',
                 'repeat_path' => "/var/lib/asterisk/sounds/defaults/repeat.wav",
                 'confirmation_path' => "/var/lib/asterisk/sounds/defaults/confirmation.wav",
-                'goodbye_path' => "/var/lib/asterisk/sounds/defaults/goodbye.wav"
+                'goodbye_path' => "/var/lib/asterisk/sounds/defaults/goodbye.wav",
+                'wrong_path' => $settings->wrong_path,
+                'success_path' => $settings->success_path,
+                'no_selection_path' => $settings->no_selection_path,
+                'selection_confirmation_path' => $settings->selection_confirmation_path,
+                'default' => $settings->default,
+                'subscription_failure_path' => $settings->subscription_failure_path,
+                'continue_path' => $settings->continue_path
             ]
         );
 
