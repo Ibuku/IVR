45111229b12aeaef349c4466f2c1e4cd653b00a6
Ademola <styccs@gmail.com>
2016-11-28 10:16:23 +0100

    updating app

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -31,29 +31,41 @@ class SettingsController extends BaseController
     public function postData($request, $response)
     {
 
-        $settings = Settings::create([
-            'advert_limit' => $request->getParam('advert_limit'),
-            'incorrect_path' => $request->getParam('incorrect_path'),
-            'repeat_path' => $request->getParam('repeat_path'),
-            'confirmation_path' => $request->getParam('confirmation_path')
-        ]);
+        $settings = Settings::first();
 
-        Index::index('settings', [
-                'advert_limit' => $settings->advert_limit,
-                'incorrect_path' => $settings->incorrect_path,
-                'repeat_path' => $settings->repeat_path,
-                'confirmation_path' => $settings->confirmation_path
-            ]
-        );
+        if ($settings) {
+            $settings->update([
+                'advert_limit' => $request->getParam('advert_limit'),
+                'incorrect_path' => $request->getParam('incorrect_path'),
+                'repeat_path' => $request->getParam('repeat_path'),
+                'confirmation_path' => $request->getParam('confirmation_path')
+            ]);
+        } else {
+            $settings = Settings::create([
+                'advert_limit' => $request->getParam('advert_limit'),
+                'incorrect_path' => $request->getParam('incorrect_path'),
+                'repeat_path' => $request->getParam('repeat_path'),
+                'confirmation_path' => $request->getParam('confirmation_path')
+            ]);
+        }
 
-        $incorrect_command = 'cp '. $settings->incorrect_path. ' '. "/var/lib/asterisk/sounds/defaults/incorrect.wav";
+        $incorrect_command = 'cp ' . $settings->incorrect_path . ' ' . "/var/lib/asterisk/sounds/defaults/incorrect.wav";
         shell_exec($incorrect_command);
 
-        $repeat_command = 'cp '. $settings->repeat_path. ' '. "/var/lib/asterisk/sounds/defaults/repeat.wav";
+        $repeat_command = 'cp ' . $settings->repeat_path . ' ' . "/var/lib/asterisk/sounds/defaults/repeat.wav";
         shell_exec($repeat_command);
 
-        $con_command = 'cp '. $settings->confirmation_path. ' '. "/var/lib/asterisk/sounds/defaults/confirmation.wav";
+        $con_command = 'cp ' . $settings->confirmation_path . ' ' . "/var/lib/asterisk/sounds/defaults/confirmation.wav";
         shell_exec($con_command);
+
+        Index::index('settings', [
+                'id' => $settings->id,
+                'advert_limit' => $settings->advert_limit,
+                'incorrect_path' => '/var/lib/asterisk/sounds/defaults/incorrect.wav',
+                'repeat_path' => "/var/lib/asterisk/sounds/defaults/repeat.wav",
+                'confirmation_path' => "/var/lib/asterisk/sounds/defaults/confirmation.wav"
+            ]
+        );
 
         return $response->withRedirect($this->router->pathFor('settings'));
 
