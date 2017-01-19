3e062d39549fbcb6c38bafda9f8898ccb9626a6b
Ademola <styccs@gmail.com>
2017-01-19 14:31:20 +0100

    updating reports flow

--- elastic-res/routes/elastic.js
+++ elastic-res/routes/elastic.js
@@ -178,7 +178,8 @@ router.post('/elasticsearch/:type/create', function (req, res, next) {
         }).then(function (resp) {
             if (resp.hits.hits.length > 0) {
                 var campaign = resp.hits.hits[0]._source;
-                var created = new Date();
+                // var created = new Date();
+                var created = new Date(new Date().getTime() - (8 * 60 * 60 * 1000));
                 created.toDateString;
 
                 client.index({
