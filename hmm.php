3cf96eaf595c06ee033307b5f0f1949c1913c8a0
Ademola <styccs@gmail.com>
2017-06-09 17:34:52 +0100

    updating...

--- hmm.php
+++ hmm.php
@@ -34,6 +34,6 @@ $_file = explode("/", $files[$file]);
 $_files = explode(".", end($_file));
 $file_path = "/var/lib/asterisk/sounds/files/" . $name . '/' . current($_files) . '.wav';
 
-$agi->set_variable('FILE_PATH', $file_path);
+$agi->set_variable('FILE_PATH', "files/" . $name . '/' . current($_files) . '.wav');
 
 return 200;
