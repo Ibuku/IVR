374b5baa33e2cd4599b56a147d553073f1c770d8
Ademola <styccs@gmail.com>
2017-06-12 12:23:37 +0100

    updating...

--- receive.js
+++ receive.js
@@ -1,25 +1,37 @@
 #!/usr/bin/env node
 var amqp = require('amqplib/callback_api');
-var request = require('request-promise');
+var rq = require('request-promise');
 
 amqp.connect('amqp://localhost', function(err, conn) {
     conn.createChannel(function(err, ch) {
         var q = 'ivr';
 
         ch.assertQueue(q, {durable: false});
-        ch.prefetch(1);
+        // ch.prefetch(1);
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
         ch.consume(q, function(msg) {
             var _data = msg.content.toString();
             var data = JSON.parse(_data);
-            request({
-                url: data.url, //URL to hit
+            var options = {
                 method: 'POST',
-                data: data
-            }, function (error, response, body) {
-                console.log(body);
-                console.log(error);
+                uri: data.url,
+                body: data,
+                json: true
+            };
+            rq(options).then(function(resp) {
+                console.log(resp);
+            })
+            .catch(function(err) {
+                console.log(err);
             });
+            // request({
+            //     url: data.url, //URL to hit
+            //     method: 'POST',
+            //     data: data
+            // }, function (error, response, body) {
+            //     console.log(body);
+            //     console.log(error);
+            // });
         }, {noAck: false});
     });
 });
