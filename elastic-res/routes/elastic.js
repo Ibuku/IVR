b6054659073dab1518fc4cea511e3e0848491229
Ademola <styccs@gmail.com>
2017-06-22 13:09:08 +0100

    updating...

--- elastic-res/routes/elastic.js
+++ elastic-res/routes/elastic.js
@@ -991,18 +991,25 @@ router.get('/data/week', function (req, res, next) {
                 var elem = total_data.data.find(function (key) {
                     return key.campaign_id === current.campaign.id
                 });
-                console.log('==========');
-                console.log(elem);
-                console.log('==========');
                 if (elem) {
-                    elem.cdr_count = parseInt(elem.cdr_count) + parseInt(current.cdr_count);
-                    elem.already_subbed_count = parseInt(elem.already_subbed_count) + parseInt(current.already_subbed_count);
-                    elem.confirmation_count = parseInt(elem.confirmation_count) + parseInt(current.confirmation_count);
-                    elem.failed_count = parseInt(elem.failed_count) + parseInt(current.failed_count);
-                    elem.impression_count = parseInt(elem.impression_count) + parseInt(current.impression_count);
-                    elem.insufficient_count = parseInt(elem.insufficient_count) + parseInt(current.insufficient_count);
-                    elem.subscription_count = parseInt(elem.subscription_count) + parseInt(current.subscription_count);
-                    elem.success_count = parseInt(elem.success_count) + parseInt(current.success_count);
+                    console.log('==========');
+                    console.log(elem.cdr_count);
+                    console.log(elem.already_subbed_count);
+                    console.log(elem.confirmation_count);
+                    console.log(elem.failed_count);
+                    console.log(elem.impression_count);
+                    console.log(elem.insufficient_count);
+                    console.log(elem.subscription_count);
+                    console.log(elem.success_count);
+                    console.log('==========');
+                    // elem.cdr_count = parseInt(elem.cdr_count) + parseInt(current.cdr_count);
+                    // elem.already_subbed_count = parseInt(elem.already_subbed_count) + parseInt(current.already_subbed_count);
+                    // elem.confirmation_count = parseInt(elem.confirmation_count) + parseInt(current.confirmation_count);
+                    // elem.failed_count = parseInt(elem.failed_count) + parseInt(current.failed_count);
+                    // elem.impression_count = parseInt(elem.impression_count) + parseInt(current.impression_count);
+                    // elem.insufficient_count = parseInt(elem.insufficient_count) + parseInt(current.insufficient_count);
+                    // elem.subscription_count = parseInt(elem.subscription_count) + parseInt(current.subscription_count);
+                    // elem.success_count = parseInt(elem.success_count) + parseInt(current.success_count);
                 }
                 else {
                     total_data.data.push(current);
